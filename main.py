35e88884e892c5fea434f1b6e783eccbd9e5c90b
Rugshan Gnanandram <rugshan@hotmail.com>
2022-07-14 00:59:26 -0400

    added countdown function to digital clock/timer

--- main.py
+++ main.py
@@ -13,11 +13,27 @@ LONG_BREAK_MIN = 20
 CANVAS_WIDTH = 200
 CANVAS_HEIGHT = 224
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def countdown_mechanism(total_seconds):
+    # Calculate Timer Minutes:Seconds
+    minutes_remaining = int(total_seconds / 60)
+    seconds_remaining = int(total_seconds % 60)
+
+    # Update Clock Text
+    canvas.itemconfigure(clock_text, text=f"{minutes_remaining:02}:{seconds_remaining:02}")
+
+    # Recursively Countdown Until 0 Seconds
+    if total_seconds > 0:
+        window.after(1000, countdown_mechanism,
+                     total_seconds - 1)  # Wait x amount of time, call function, passing it an argument (*args).
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -28,29 +44,35 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 # Timer Title
 title_label = Label(text="Timer", font=(FONT_NAME, 38, "bold"), bg=YELLOW, fg=GREEN)
-title_label.grid(row=1, column=2)
+title_label.grid(row=0, column=1, sticky="N")
 
 # Canvas to Layer Widgets
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="tomato.png")
 canvas.create_image((CANVAS_WIDTH / 2), (CANVAS_HEIGHT / 2), image=tomato_image)
-canvas.grid(row=2, column=2)
+canvas.grid(row=1, column=1)
 
-# Clock Text ✔
-canvas.create_text((CANVAS_WIDTH / 2),
-                   ((CANVAS_HEIGHT / 2) + 30),
-                   text="00:00",
-                   fill="white",
-                   font=(FONT_NAME, 35, "bold"))
+# Clock Text
+clock_text = canvas.create_text((CANVAS_WIDTH / 2),
+                                ((CANVAS_HEIGHT / 2) + 30),
+                                text="00:00",
+                                fill="white",
+                                font=(FONT_NAME, 35, "bold"))
+
+# Update Digital Clock
+countdown_mechanism(10)
 
 # Start Button
 start_button = Button(text="Start")
-start_button.grid(row=3, column=1, sticky="E")
-
+start_button.grid(row=2, column=0, sticky="E")
 
 # Reset Button
 start_button = Button(text="Reset")
-start_button.grid(row=3, column=3, sticky="W")
+start_button.grid(row=2, column=2, sticky="W")
+
+# Checkmarks Label
+checkmarks_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+checkmarks_label.grid(row=3, column=1)
 
 # Window Main Loop
 window.mainloop()
